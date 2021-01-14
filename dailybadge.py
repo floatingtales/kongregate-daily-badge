f338af31af3f0b3d1ac9cfb025a569f0fc0c4774
floatingtales <dillan.martin@ymail.com>
2021-01-15 05:26:45 +0800

    Runs the program every single day at 5pm
    A straightforward project, that opens kongregate on your browser, find the daily badge and opens it every day at 5pm

---
+++ dailybadge.py
@@ -0,0 +1,36 @@
+import schedule
+import time
+import webbrowser
+import ssl
+import re
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+kongpanionurl = 'https://www.kongregate.com/kongpanions'
+
+def badge():
+
+    print('retreiving:', kongpanionurl)
+
+    html = urllib.request.urlopen(kongpanionurl, context = ctx).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    dayblob = str(soup('dt', class_='day click_box today'))
+
+    gameurl = re.findall('www.kongregate.com/[^\"]+', dayblob)
+
+    print("opening:", gameurl)
+
+    webbrowser.open(gameurl[0],2)
+
+#scheduler
+schedule.every().day.at("17:00").do(badge)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
